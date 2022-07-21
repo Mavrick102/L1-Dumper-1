fb3e505e25e9272bfa90fd1ec066c7e1e19f7aea
t33v33b0x <96581227+t33v33b0x@users.noreply.github.com>
2022-07-21 17:20:11 +0100

    Update L1-Dumper.py

--- L1-Dumper.py
+++ L1-Dumper.py
@@ -2,50 +2,6 @@ import time
 
 print('\n***L1-DUMPER 7.2***\n\n')
 
-time.sleep(3)
-
-print('Device Connecting...')
-
-time.sleep(1)
-
-print('Connected...')
-
-time.sleep(1)
-
-print('Checking Root...')
-
-time.sleep(2)
-
-print('Device not Rooted...')
-
-time.sleep(2)
-
-print('Starting The Script...')
-
-time.sleep(2)
-
-print('Play DRM Video In The Connected Device...')
-
-time.sleep(1)
-
-print('Licence Request Created...')
-
-print('.....10%')
-
-time.sleep(2)
-
-print('........70%')
-
-time.sleep(1)
-
-print('...........100%\n\n')
-
-time.sleep(2)
-
-print('Keybox Extraction Done...')
-print('Key Extraction Complete...')
-print('Dumped To Downloads...\n\n')
-
 time.sleep(1)
 
 print('DONT BE STUPID - DONT GET SCAMMED :)\n\n')
